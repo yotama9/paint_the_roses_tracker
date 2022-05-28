ed687fc746425b64919ec45ef4c1e5e858239fdb
yotama9 <yotama9@gmail.com>
2022-05-28 21:18:47 +0300

    Updated the js.
    This now creates all required functions for real use (at least until someone has some comments for me).

--- scripts/funcs.js
+++ scripts/funcs.js
@@ -1,3 +1,42 @@
-$(document).ready(function(){
-        $("#test").hide();
-});
+<!DOCTYPE html>
+<head>
+	<title>Unofficial paint the roses tracker</title>
+	<link rel="stylesheet" href="css/s.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="scripts/funcs.js"</script>
+	<script>
+	</script>
+</head>
+
+<body>
+
+  <h2>Welcome to my  unofficial paint the roses tracker</h2>
+  <h3>Disclaimers</h3>
+  <div>
+    This is a tracker I've developed for the game after noticing how fast the paper tracker are consumed. It should be fairly easy to use. 
+  </div>
+
+  <div>
+    For information about the game, please go to the <a target="_blank" href="https://www.northstargames.com/pages/paint-the-roses">game's webpage</a>.
+  </div>
+
+  <div>
+    This is an open srouce page, developed and share under GPL license terms. Found an error? Want to add/propose changes? Please go to the <a target="_blank" href="https://github.com/yotama9/ptrt">github page</a>.
+  </div>
+
+  <div>
+    This page is developed by myself on my spare time. As such, it might have errors. Yes, I know, it is not very pretty. You are welcome to propose changes. 
+  </div>
+
+  <div>
+    All icons in this page are taken from <a target="_blank" href="https://flaticons.net">Flaticons</a>. As per the disclaimer on their page, they are royalty free.
+  </div>
+
+  
+
+  <hr>
+  How many players are there in your game?: <input type="number" step="1" value="2" min="2" max="5" id="num_players"></input>
+    
+    <span id="place_for_tables">
+</body>
+
